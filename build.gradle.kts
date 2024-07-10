db505c253f8a736a8d262762464308a28a9902d1
Limbyc <124778926+Limbyc@users.noreply.github.com>
2024-07-10 11:41:24 +0300

    #2: fix commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,6 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.google.gms.google.services) apply false
+    alias(libs.plugins.google.firebase.crashlytics) apply false
 }
